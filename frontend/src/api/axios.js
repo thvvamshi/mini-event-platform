5cdf36f72e4db732a086471f2a2fbce0add14d33
thvvamshi <bodavamshikumar2002@gmail.com>
2025-12-31 14:39:25 +0530

    connected backend & frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
-// 🔴 Automatically attach JWT to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
